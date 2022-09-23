21a698b794cedc87ffdde37e870e9c6a7b62ba1e
Mo Zakir <100852006+Mozakir178@users.noreply.github.com>
2022-09-23 17:47:46 +0530

    Create problem1.js

---
+++ SpeedTest/problem1.js
@@ -0,0 +1,11 @@
+function stockItUp(N,elements){
+    let arr = [110*5, 120*2 , 42*2 ,53*3 , 40*2  ,32*5 , 126*2] ;
+    let sum = 0 ;
+    for(let i=0;i<7;i++){
+        if(elements[i] == 1){
+            sum+= arr[i] ;
+        }
+    }
+    console.log(sum*N) ;
+  
+}
